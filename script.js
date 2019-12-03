8b5dbb6155ede9ab0c2826d6a8ea0a6ce590f9a5
NSarg7 <NSarg7@users.noreply.github.com>
2019-12-03 17:13:33 +0400

    setTimeout changed with transitionend and transitioncancel.

--- script.js
+++ script.js
@@ -1,98 +1,93 @@
 'use strict';
 const data = {
-    num: 0,
-    itemIndex: 5,
-    clickActive: true,
-    nodeArr: Array.from(document.querySelectorAll('.pic')),
+    nodeList: document.querySelectorAll('.pic'),
     btnNext: document.querySelector('.btn-next'),
     btnPrev: document.querySelector('.btn-prev'),
     item: document.querySelector('.pic'),
+    num: 0,
+    itemIndex: 5,
+    clickActive: true,
     widthComp: function () {
-        return getComputedStyle(this.item).getPropertyValue('width')
+        return getComputedStyle(this.item).getPropertyValue('width');
     },
     marginComp: function () {
-        return getComputedStyle(this.item).getPropertyValue('margin')
-    },
+        return getComputedStyle(this.item).getPropertyValue('margin');
+    }
 };
 
 const slideWidth = parseInt(data.widthComp(), 10) + parseInt(data.marginComp(), 10) * 2;
+
+ 
+function translateToZero() {
+    console.log('transitionend');
+    for (const el of data.nodeList) {
+        el.style.transition = 'transform 0s';
+        el.style.transform = `translate(${0}px)`;
+    }
+    data.num = 0;
+    data.itemIndex = 5;
+}
+
+const resetTransition = () => {
+    console.log('transitionCancel');
+    for (const el of data.nodeList) {
+        el.style.transition = 'transform 0.3s';
+    }
+};
 
 const btnNextMethod = () => {
     if (data.itemIndex < 11) {
         data.itemIndex++;
     }
-
     data.num -= slideWidth;
-    data.nodeArr.forEach(el => {
+    for (const el of data.nodeList) {
         el.style.transform = `translate(${data.num}px)`;
-    });
+    }
 
     if (data.itemIndex === 10) {
-        setTimeout(() => {
-            data.nodeArr.forEach(el => {
-                el.style.transition = 'all 0s';
-                el.style.transform = `translate(${0}px)`;
-
-            })
-            data.num = 0;
-        }, 500);
-        setTimeout(() => {
-            data.nodeArr.forEach(el => {
-                el.style.transition = 'transform 0.5s';
-            })
-        }, 520);
-        data.itemIndex = 5;
+        data.item.addEventListener('transitionend', translateToZero);
+        data.item.addEventListener('transitioncancel', resetTransition);
+    } else {
+        data.item.removeEventListener('transitionend', translateToZero);
     }
     data.clickActive = false;
+    console.log(data.itemIndex);
 };
-
-data.btnNext.addEventListener('click', () => {
-    if (data.clickActive) {
-        requestAnimationFrame(btnNextMethod);
-
-        setTimeout(() => {
-            data.clickActive = true;
-        }, 600);
-    }
-});
 
 const btnPrevMethod = () => {
     if (data.itemIndex > 0) {
         data.itemIndex--;
     }
-
     data.num += slideWidth;
-
-    data.nodeArr.forEach(el => {
+    for (const el of data.nodeList) {
         el.style.transform = `translate(${data.num}px)`;
-    });
+    }
 
     if (data.itemIndex === 0) {
-        setTimeout(() => {
-            data.nodeArr.forEach(el => {
-                el.style.transition = 'all 0s';
-                el.style.transform = `translate(${0}px)`;
-            });
-            data.num = 0;
-        }, 500);
-
-        setTimeout(() => {
-            data.nodeArr.forEach(el => {
-                el.style.transition = 'transform 0.5s';
-            });
-
-        }, 520);
-        data.itemIndex = 5;
+        data.item.addEventListener('transitionend', translateToZero);
+        data.item.addEventListener('transitioncancel', resetTransition);
+    } else {
+        data.item.removeEventListener('transitionend', translateToZero);
     }
     data.clickActive = false;
 };
 
+
+// BUTTON LISTENERS
+data.btnNext.addEventListener('click', () => {
+    if (data.clickActive) {
+        requestAnimationFrame(btnNextMethod);
+        data.item.addEventListener('transitionend', () => {
+            data.clickActive = true;
+        });
+    }
+});
+
 data.btnPrev.addEventListener('click', () => {
     if (data.clickActive) {
         requestAnimationFrame(btnPrevMethod);
-
-        setTimeout(() => {
+        data.item.addEventListener('transitionend', () => {
             data.clickActive = true;
-        }, 600);
+        });
     }
 });
